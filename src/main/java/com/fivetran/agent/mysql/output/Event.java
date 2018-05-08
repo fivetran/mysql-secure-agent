95b3399007af401a9d4adb7f6eea0ff70e61c4b2
glarwood <gabriel@fivetran.com>
2018-05-07 23:21:31 -0700

    cleanup(Event): simplify constructor parameters

--- src/main/java/com/fivetran/agent/mysql/output/Event.java
+++ src/main/java/com/fivetran/agent/mysql/output/Event.java
@@ -30,34 +30,34 @@ public class Event {
     }
 
     public static Event createUpsert(TableRef tableRef, List<String> row) {
-        return new Event(tableRef, new Upsert(tableRef, row));
+        return new Event(new Upsert(tableRef, row));
     }
 
     public static Event createDelete(TableRef tableRef, List<String> row) {
-        return new Event(tableRef, new Delete(tableRef, row));
+        return new Event(new Delete(tableRef, row));
     }
 
     public static Event createTableDefinition(TableDefinition tableDefinition) {
         return new Event(tableDefinition);
     }
 
-    Event(TableRef tableRef, Upsert upsert) {
-        this.tableRef = tableRef;
+    private Event(Upsert upsert) {
+        this.tableRef = upsert.table;
         this.tableDefinition = Optional.empty();
         this.upsert = Optional.of(upsert);
         this.delete = Optional.empty();
         this.eventType = UPSERT;
     }
 
-    Event(TableRef tableRef, Delete delete) {
-        this.tableRef = tableRef;
+    private Event(Delete delete) {
+        this.tableRef = delete.table;
         this.tableDefinition = Optional.empty();
         this.upsert = Optional.empty();
         this.delete = Optional.of(delete);
         this.eventType = DELETE;
     }
 
-    Event(TableDefinition tableDefinition) {
+    private Event(TableDefinition tableDefinition) {
         this.tableRef = tableDefinition.table;
         this.tableDefinition = Optional.of(tableDefinition);
         this.upsert = Optional.empty();
@@ -65,7 +65,7 @@ public class Event {
         this.eventType = TABLE_DEFINITION;
     }
 
-    Event() {
+    private Event() {
         this.tableRef = new TableRef("", "");
         this.tableDefinition = Optional.empty();
         this.upsert = Optional.empty();
