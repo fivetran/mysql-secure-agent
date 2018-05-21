252f320963cf3e01d454765068d00a47c3a98e3b
glarwood <gabriel@fivetran.com>
2018-05-21 16:57:22 -0700

    refactor(TableState): overwrite equals & hashCode methods

--- src/main/java/com/fivetran/agent/mysql/state/TableState.java
+++ src/main/java/com/fivetran/agent/mysql/state/TableState.java
@@ -4,9 +4,25 @@
 package com.fivetran.agent.mysql.state;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 public class TableState {
     public Optional<Map<String, String>> lastSyncedPrimaryKey = Optional.empty();
     public boolean finishedImport = false;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TableState that = (TableState) o;
+        return finishedImport == that.finishedImport &&
+                Objects.equals(lastSyncedPrimaryKey, that.lastSyncedPrimaryKey);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(lastSyncedPrimaryKey, finishedImport);
+    }
 }
